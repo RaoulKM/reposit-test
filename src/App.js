03b9037b2c205a4238d605ff8a20a36d6e77056f
RaoulKM <raoulkm2006@gmail.com>
2025-05-07 12:32:22 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import './style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <header>
+            <nav>
+    
+                <h1>RaoulKM <i class="fa fa-fire"></i></h1>
+                
+                <ul>
+    
+                    <li><a class="fa fa-home" href="">Home</a></li>
+                    <li><a class="fa fa-help" href="">About me?</a></li>
+                    <li><a class="fa fa-road" href="">My road</a></li>
+                    <li><a class="fa fa-laptop" href="">Skills</a></li>
+                    <li><a class="fa fa-book" href="">Project</a></li>
+                    <li><a class="fa fa-phone" href="">Contact</a></li>
+    
+                </ul>
+            </nav>
+            <div class="sect-text">
+                <div class="hello">
+                    <span>H</span>ello <i class="fa fa-eyes"></i>
+                </div>
+    
+                <div class="my-name">I'm <div class="name">Raoul KOM</div> </div>
+    
+                <div class="grade">
+                    WEB and GRAPHIC DESIGNER
+                </div>
+            </div>
+        </header>
   );
 }
 
